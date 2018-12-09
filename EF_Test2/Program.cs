8c7eb50deea5d8ab6a64a39b0de8c6b90e83c0a1
Maciej <maciej.jarzebkowski@gmail.com>
2018-12-09 21:00:43 +0100

    Ok, działa

--- EF_Test2/Program.cs
+++ EF_Test2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace EF_Test2
     {
         static void Main(string[] args)
         {
+            var context = new DatabaseFirstDemoEntities();
+            var post = new Table_1()
+            {
+                Body = "Body",
+                DatePublished = DateTime.Now,
+                Title = "Title",
+                PostID = 1
+            };
+            context.Table_1.Add(post);
+            context.SaveChanges();
+
         }
     }
 }
